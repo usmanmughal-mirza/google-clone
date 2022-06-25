a11eb415567195ed32210e3266bf73d9cf2d94cb
usmanmughal-mirza <ubzafar2001@gmail.com>
2022-06-24 20:42:11 -0700

    files added

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Hearts} from 'react-loader-spinner';
+
+export const Loading = () => (
+  <div className="flex justify-center items-center ">
+    <Hearts 
+     color="#00BFFF" height={550} width={80} />
+  </div>
+);
